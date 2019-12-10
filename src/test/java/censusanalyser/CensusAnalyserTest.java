adb2066eb4b0f52cfd49f3affbc332ae4f10d2e1
mvinchankar <mvinchankar18@gmail.com>
2019-12-10 16:20:25 +0530

    ADDED : test case when sorted state return unsorted list

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -66,6 +66,20 @@ public class CensusAnalyserTest {
     }
 
     @Test
+    public void givenIndianCensusData_whenSortedOnState_ShouldReturnUnSortedResult() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        String stateWiseSortedCensusData = null;
+        try {
+            censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            stateWiseSortedCensusData = censusAnalyser.getStateWiseSortedCensusData();
+            IndiaCensusDAO[] censusCSV = new Gson().fromJson(stateWiseSortedCensusData, IndiaCensusDAO[].class);
+            Assert.assertNotEquals("Uttar Pradesh", censusCSV[0].state);
+        } catch (CSVBuilderException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void givenIndianCensusCSVFile_WhenIncorrectFilePath_ShouldReturnsException() {
         try {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
